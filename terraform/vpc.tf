352c28b956d3364da4d6e49a8ce8d71413ff0c53
JobinAj <jobinaj6361@gmail.com>
2025-12-31 17:47:04 +0530

    fix: by miss i added the 'us-east-1a' in the aws region field now i have added it to the az's field

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name="${var.cluster_name}-vpc"
   cidr=var.vpc_cidr
 
-azs             = ["us-east-1"]
+azs             = ["us-east-1a"]
 private_subnets = ["10.0.1.0/24"]
 public_subnets  = ["10.0.101.0/24"]
 
