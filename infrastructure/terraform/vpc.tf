a4e3b878bbb6435d1c62d806ee41ce0e242fb89a
JobinAj <jobinaj6361@gmail.com>
2026-01-13 07:11:28 +0530

    fix:I modified infrastructure/terraform/vpc.tf to add explicit VPC Endpoints for: com.amazonaws.us-east-1.ecr.api com.amazonaws.us-east-1.ecr.dkr com.amazonaws.us-east-1.s3

--- infrastructure/terraform/vpc.tf
+++ infrastructure/terraform/vpc.tf
@@ -99,10 +99,63 @@ resource "aws_vpc_endpoint" "kms" {
   }
 }
 
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id            = module.vpc.vpc_id
+  service_name      = "com.amazonaws.${var.aws_region}.ecr.api"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = module.vpc.private_subnets
+
+  security_group_ids = [
+    aws_security_group.vpc_endpoint_sg.id
+  ]
+
+  private_dns_enabled = true
+  tags = {
+    Name        = "${var.cluster_name}-ecr-api-vpc-endpoint"
+    Environment = var.environment
+  }
+}
+
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id            = module.vpc.vpc_id
+  service_name      = "com.amazonaws.${var.aws_region}.ecr.dkr"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = module.vpc.private_subnets
+
+  security_group_ids = [
+    aws_security_group.vpc_endpoint_sg.id
+  ]
+
+  private_dns_enabled = true
+  tags = {
+    Name        = "${var.cluster_name}-ecr-dkr-vpc-endpoint"
+    Environment = var.environment
+  }
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = module.vpc.vpc_id
+  service_name      = "com.amazonaws.${var.aws_region}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = module.vpc.private_route_table_ids
+
+  tags = {
+    Name        = "${var.cluster_name}-s3-vpc-endpoint"
+    Environment = var.environment
+  }
+}
+
 resource "aws_security_group" "vpc_endpoint_sg" {
   name_prefix = "${var.cluster_name}-vpc-endpoint-sg"
   description = "Security group for VPC endpoints"
   vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
 
   tags = {
     Name        = "${var.cluster_name}-vpc-endpoint-sg"
